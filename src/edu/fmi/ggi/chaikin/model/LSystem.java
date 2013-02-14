5cd51855620873a196c896a45dbdad667ac4e6cb
martin <asenov.m@gmail.com>
2013-02-14 23:50:19 +0200

    extracted the L-systems related logic into a separate class

---
+++ src/edu/fmi/ggi/chaikin/model/LSystem.java
@@ -0,0 +1,135 @@
+package edu.fmi.ggi.chaikin.model;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LSystem {
+
+	/**
+	 * {@value}
+	 */
+	private static final String PATTERN_SPLIT_POINT = " ";
+
+	/**
+	 * {@value}
+	 */
+	private static final String PATTERN_REPLACE_POINT = "(\\d+) (\\d+) (\\d+) (\\d+) (\\d+) (\\d+)";
+
+	/**
+	 * The pattern to be used for replacing the points in our polygon
+	 */
+	private static final Pattern PATTERN_L_SYSTEM;
+
+	static {
+		PATTERN_L_SYSTEM = Pattern.compile(PATTERN_REPLACE_POINT);
+	}
+
+	private final String input;
+
+	/**
+	 * Construct a new L-system that will parse the <tt>input</tt> string using
+	 * its internal rules
+	 * 
+	 * @param input
+	 *            the input string to be parsed, using the rules of the l-system
+	 */
+	public LSystem(final String input) {
+		this.input = input;
+	}
+
+	/**
+	 * Computes the coordinates of the points that are created as a result of
+	 * applying the rules of the L-system to the given input string
+	 * 
+	 * @return the points that are created as a result of the application of the
+	 *         rules of the L-system on the given input string
+	 */
+	public Collection<Point> expand() {
+		return smoothenEdges(input);
+	}
+
+	private Collection<Point> smoothenEdges(final String vertexes) {
+		final Matcher matcher = PATTERN_L_SYSTEM.matcher(vertexes);
+
+		final StringBuilder builder = new StringBuilder();
+
+		while (matcher.find()) {
+			String[] splitGroup = matcher.group().split(PATTERN_SPLIT_POINT);
+
+			final Point start = getStartPoint(splitGroup);
+			final Point middle = getMiddlePoint(splitGroup);
+			final Point end = getEndPoint(splitGroup);
+
+			final Point newStartPoint = getNewStartPoint(start, middle);
+			final Point newEndPoint = getNewEndPoint(middle, end);
+
+			appendPoint(builder, newStartPoint);
+			appendPoint(builder, newEndPoint);
+
+			matcher.region(getMatcherStart(matcher), getMatcherEnd(matcher));
+		}
+
+		return parseAndAddPoints(builder.toString());
+	}
+
+	private void appendPoint(final StringBuilder builder, final Point point) {
+		builder.append(point.x);
+		builder.append(" ");
+		builder.append(point.y);
+		builder.append(" ");
+	}
+
+	private Collection<Point> parseAndAddPoints(final String points) {
+		final List<Point> result = new ArrayList<Point>();
+
+		final String[] stringPoints = points.split(PATTERN_SPLIT_POINT);
+		for (int i = 0; i < stringPoints.length - 1; i += 2) {
+			final int x = Integer.parseInt(stringPoints[i]);
+			final int y = Integer.parseInt(stringPoints[i + 1]);
+			result.add(new Point(x, y));
+		}
+
+		final Point first = result.get(0);
+		result.add(first);
+
+		return result;
+	}
+
+	private Point getNewEndPoint(final Point middle, final Point end) {
+		return new Point((int) Math.round(0.75 * middle.x + 0.25 * end.x),
+				(int) Math.round(0.75 * middle.y + 0.25 * end.y));
+	}
+
+	private Point getNewStartPoint(final Point start, final Point middle) {
+		return new Point((int) Math.round(0.25 * start.x + 0.75 * middle.x),
+				(int) Math.round(0.25 * start.y + 0.75 * middle.y));
+	}
+
+	private Point getEndPoint(String[] splitGroup) {
+		return new Point(Integer.parseInt(splitGroup[4]),
+				Integer.parseInt(splitGroup[5]));
+	}
+
+	private Point getMiddlePoint(String[] splitGroup) {
+		return new Point(Integer.parseInt(splitGroup[2]),
+				Integer.parseInt(splitGroup[3]));
+	}
+
+	private Point getStartPoint(String[] splitGroup) {
+		return new Point(Integer.parseInt(splitGroup[0]),
+				Integer.parseInt(splitGroup[1]));
+	}
+
+	private int getMatcherEnd(final Matcher matcher) {
+		return matcher.regionEnd();
+	}
+
+	private int getMatcherStart(final Matcher matcher) {
+		return matcher.regionStart() + matcher.group(1).length()
+				+ matcher.group(2).length() + 2;
+	}
+}
--- src/edu/fmi/ggi/chaikin/model/Polygon.java
+++ src/edu/fmi/ggi/chaikin/model/Polygon.java
@@ -6,35 +6,14 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import edu.fmi.ggi.chaikin.listeners.DrawingObserver;
 
 public class Polygon {
 
-	/**
-	 * {@value}
-	 */
-	private static final String PATTERN_SPLIT_POINT = " ";
-
-	/**
-	 * {@value}
-	 */
-	private static final String PATTERN_REPLACE_POINT = "(\\d+) (\\d+) (\\d+) (\\d+) (\\d+) (\\d+)";
-
-	/**
-	 * The pattern to be used for replacing the points in our polygon
-	 */
-	private static final Pattern PATTERN_L_SYSTEM;
-
 	private final Set<DrawingObserver> observers;
 
 	private final List<Point> points;
-
-	static {
-		PATTERN_L_SYSTEM = Pattern.compile(PATTERN_REPLACE_POINT);
-	}
 
 	/**
 	 * Constructs a new empty polygon
@@ -101,7 +80,7 @@ public class Polygon {
 		final String vertexes = this.toString();
 
 		points.clear();
-		smoothenEdges(vertexes);
+		points.addAll(new LSystem(vertexes).expand());
 
 		notifyObservers();
 	}
@@ -123,86 +102,9 @@ public class Polygon {
 		return builder.toString();
 	}
 
-	private Point getNewEndPoint(final Point middle, final Point end) {
-		return new Point((int) Math.round(0.75 * middle.x + 0.25 * end.x),
-				(int) Math.round(0.75 * middle.y + 0.25 * end.y));
-	}
-
-	private Point getNewStartPoint(final Point start, final Point middle) {
-		return new Point((int) Math.round(0.25 * start.x + 0.75 * middle.x),
-				(int) Math.round(0.25 * start.y + 0.75 * middle.y));
-	}
-
-	private Point getEndPoint(String[] splitGroup) {
-		return new Point(Integer.parseInt(splitGroup[4]),
-				Integer.parseInt(splitGroup[5]));
-	}
-
-	private Point getMiddlePoint(String[] splitGroup) {
-		return new Point(Integer.parseInt(splitGroup[2]),
-				Integer.parseInt(splitGroup[3]));
-	}
-
-	private Point getStartPoint(String[] splitGroup) {
-		return new Point(Integer.parseInt(splitGroup[0]),
-				Integer.parseInt(splitGroup[1]));
-	}
-
-	private int getMatcherEnd(final Matcher matcher) {
-		return matcher.regionEnd();
-	}
-
-	private int getMatcherStart(final Matcher matcher) {
-		return matcher.regionStart() + matcher.group(1).length()
-				+ matcher.group(2).length() + 2;
-	}
-
 	private void notifyObservers() {
 		for (final DrawingObserver observer : observers) {
 			observer.onModelChanged(Collections.unmodifiableList(points));
 		}
 	}
-
-	private void smoothenEdges(final String vertexes) {
-		final Matcher matcher = PATTERN_L_SYSTEM.matcher(vertexes);
-
-		final StringBuilder builder = new StringBuilder();
-
-		while (matcher.find()) {
-			String[] splitGroup = matcher.group().split(PATTERN_SPLIT_POINT);
-
-			final Point start = getStartPoint(splitGroup);
-			final Point middle = getMiddlePoint(splitGroup);
-			final Point end = getEndPoint(splitGroup);
-
-			final Point newStartPoint = getNewStartPoint(start, middle);
-			final Point newEndPoint = getNewEndPoint(middle, end);
-
-			appendPoint(builder, newStartPoint);
-			appendPoint(builder, newEndPoint);
-
-			matcher.region(getMatcherStart(matcher), getMatcherEnd(matcher));
-		}
-
-		parseAndAddPoints(builder.toString());
-	}
-
-	private void appendPoint(final StringBuilder builder, final Point point) {
-		builder.append(point.x);
-		builder.append(" ");
-		builder.append(point.y);
-		builder.append(" ");
-	}
-
-	private void parseAndAddPoints(final String points) {
-		final String[] stringPoints = points.split(" ");
-		for (int i = 0; i < stringPoints.length - 1; i += 2) {
-			final int x = Integer.parseInt(stringPoints[i]);
-			final int y = Integer.parseInt(stringPoints[i + 1]);
-			this.points.add(new Point(x, y));
-		}
-
-		this.points.add(this.points.get(0));
-	}
-
 }
